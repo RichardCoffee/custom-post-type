cc0f056106a4eb2fce95217341872d8998538dba
Laptop 3 <richard.coffee@gmail.com>
2017-01-11 07:42:22 -0500

    change method name

--- classes/Trait/Singleton.php
+++ classes/Trait/Singleton.php
@@ -4,7 +4,7 @@ trait TCC_Trait_Singleton {
 
 	private static $instance;
 
-	public static function getInstance($args=array()) {
+	public static function get_instance($args=array()) {
 		if (!(self::$instance instanceof self)) {
 			self::$instance = new self($args);
 		}
