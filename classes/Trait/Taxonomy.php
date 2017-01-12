414b3881a120bd89d8ff7b7c3002705a0f75338a
Laptop 3 <richard.coffee@gmail.com>
2017-01-12 05:54:58 -0500

    simplify method get_taxonomy_label

--- classes/Trait/Taxonomy.php
+++ classes/Trait/Taxonomy.php
@@ -3,14 +3,13 @@
 trait TCC_Trait_Taxonomy {
 
 	protected function get_taxonomy_label($tax,$label) {
-		$return = '';
-		if (taxonomy_exists($tax) {
-			$labels = get_taxonomy($tax)->labels;
+		if (taxonomy_exists($tax)) {
+			$labels = get_taxonomy_labels($tax);
 			if (!empty($labels->$label)) {
-				$return = $labels->$label;
+				return $labels->$label;
 			}
 		}
-		return $return;
+		return '';
 	}
 
 }
