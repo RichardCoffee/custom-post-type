6aacea5a55920f5cbfe5c10f16ac3fb6dfe33d9a
Laptop 3 <richard.coffee@gmail.com>
2017-01-13 02:59:41 -0500

    add url and __call methods

--- classes/Plugin/Paths.php
+++ classes/Plugin/Paths.php
@@ -2,15 +2,23 @@
 
 class TCC_Plugin_Paths {
 
-	public $file;
 	public $dir;
 	public $url;
 
 	use TCC_Trait_Singleton;
 
 	public function __construct($args) {
-log_entry($args);
 		$this->dir = $args['path'];
+		$this->url = $args['url'];
+	}
+
+	public function __call($name,$arguments) {
+		if (property_exists($this,$name)) return $this->name;
+		return null;
+	}
+
+	public static function url($file='') {
+		return $this->url . $file;
 	}
 
 }
