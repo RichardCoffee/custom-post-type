2b622d898e3b6c568c541a2ccd296145ae131463
Laptop 3 <richard.coffee@gmail.com>
2017-05-05 04:30:07 -0400

    add base Options class

---
+++ classes/Options/Options.php
@@ -0,0 +1,60 @@
+<?php
+
+abstract class TCC_Options_Options {
+
+
+	protected $base     = 'options'; # change this in child
+	protected $priority = 1000;      # change this in child
+	protected $screen   = array();
+
+	abstract protected function form_title();
+	abstract protected function describe_options();
+	abstract protected function options_layout();
+
+
+	public function __construct() {
+		add_filter( 'fluidity_options_form_layout',        array( $this, 'form_layout' ),          $this->priority );
+		add_filter( 'tcc_form_admin_options_localization', array( $this, 'options_localization' ), $this->priority );
+#		add_action( 'fluid-customizer', array( $this, 'options_customize_register' ), $this->priority, 2 );
+	}
+
+	public function form_layout( $form ) {
+		if ( ! isset( $form[ $this->base ] ) ) {
+			$form[ $this->base ] = ( empty( $this->screen ) ) ? $this->default_form_layout() : $this->screen;
+		}
+		return $form;
+	}
+
+	public function default_form_layout() {
+		$this->screen = array(
+			'describe' => array( $this, 'describe_options' ),
+			'title'    => $this->form_title(),
+			'option'   => 'tcc_options_'.$this->base,
+			'layout'   => $this->layout,
+		);
+		return $this->screen;
+	}
+
+	public function options_localization( $data = array() ) {
+		foreach( $this->layout as $key => $item ) {
+			if ( isset( $item['showhide'] ) ) {
+				$data[] = $item['showhide'];
+			}
+		}
+log_entry($data);
+		return $data;
+	}
+
+	public function get_default_options() {
+		$form = $this->options_layout( true );
+		$opts = array();
+		foreach( $form as $key => $option ) {
+			if ( isset( $option['default'] ) ) {
+				$opts[ $key ] = $option['default'];
+			}
+		}
+		return $opts;
+	}
+
+
+}
