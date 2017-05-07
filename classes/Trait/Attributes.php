5e6c3f2079a689fb2d0f1a7d9bc4cfef651e9fa8
Laptop 3 <richard.coffee@gmail.com>
2017-05-07 19:21:16 -0400

    add methods apply_attrs_tag, get_apply_attrs_tag, is_self_closing, remove method get_apply_attrs_nav

--- classes/Trait/Attributes.php
+++ classes/Trait/Attributes.php
@@ -7,6 +7,10 @@ trait TCC_Trait_Attributes {
 		echo $this->get_apply_attrs( $args );
 	}
 
+	public function apply_attrs_tag( $attrs, $html_tag ) {
+		echo $this->get_apply_attrs_tag( $attrs, $html_tag );
+	}
+
 	public function get_apply_attrs( $args ) {
 		$attrs = ' ';
 		foreach( $args as $attr => $value ) {
@@ -33,8 +37,15 @@ trait TCC_Trait_Attributes {
 		return $attrs;
 	}
 
-	public function get_apply_attrs_nav( $args ) {
-		return '<nav ' . $this->get_apply_attrs( $args ) . '>';
+	public function get_apply_attrs_tag( $attrs, $html_tag ) {
+		$html = '<' . $html_tag . $this->get_apply_attrs( $attrs );
+		$html .= ( $this->is_self_closing( $html_tag ) ) ? ' />' : '>';
+		return $html;
+	}
+
+	private function is_self_closing( $tag ) {
+		$self_closing = array( 'area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr' );
+		return in_array( $tag, $self_closing, true );
 	}
 
 
