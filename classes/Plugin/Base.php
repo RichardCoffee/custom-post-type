fec0886479587da70a530c839684d9f533d261f2
Laptop 3 <richard.coffee@gmail.com>
2018-04-04 22:23:44 -0400

    add basic template class for plugins

---
+++ classes/Plugin/Base.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * classes/Plugin/Base.php
+ *
+ */
+/**
+ * Main plugin class
+ *
+ */
+class TCC_Plugin_Base extends TCC_Plugin_Plugin {
+
+
+	public function initialize() {
+		if ( ( ! TCC_Register_Register::php_version_check() ) || ( ! TCC_Register_Register::wp_version_check() ) ) {
+			return
+		}
+		register_deactivation_hook( $this->paths->file, array( 'TCC_Register_Register', 'deactivate' ) );
+		register_uninstall_hook(    $this->paths->file, array( 'TCC_Register_Register', 'uninstall'  ) );
+		$this->add_actions();
+		$this->add_filters();
+	}
+
+	public function add_actions() {
+		parent::add_actions();
+	}
+
+	public function add_filters() {
+		parent::add_filters();
+	}
+
+}
