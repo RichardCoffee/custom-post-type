e6c1d3a488f933dfed31662c4ef969041ca0c78f
Richard Coffee <richard.coffee@gmail.com>
2019-12-02 01:01:05 -0500

    add class Enum_English

---
+++ classes/Enum/English.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ *  classes/Enum/English.php
+ *
+ */
+
+class TCC_Enum_English extends TC_Enum_Enum {
+
+
+	use DND_Trait_Singleton;
+
+
+	protected function __construct( $args = array() ) {
+		$this->set = array( 'Zero',
+			'One',           'Two',          'Three',        'Four',        'Five',
+			'Six',           'Seven',        'Eight',        'Nine',        'Ten',
+			'Eleven',        'Twelve',       'Thirteen',     'Fourteen',    'Fifteen',
+			'Sixteen',       'Seventeen',    'Eighteen',     'Nineteen',    'Twenty',
+			'Twenty-One',    'Twenty-Two',   'Twenty-Three', 'Twenty-Four', 'Twenty-Five',
+			'Twenty-Six',    'Twenty-Seven', 'Twenty-Eight', 'Twenty-Nine', 'Thirty',
+		);
+		if ( $args && is_array( $args ) ) $this->set = array_replace( $this->set, $args );
+	}
+
+}
