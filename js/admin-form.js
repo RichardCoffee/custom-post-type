6635abee940361d77791e4f252b3c0e42604d8f2
Richard Coffee <richard.coffee@gmail.com>
2020-04-10 22:44:08 -0400

    replace var keyword

--- js/admin-form.js
+++ js/admin-form.js
@@ -4,7 +4,7 @@ jQuery( document ).ready( function() {
 	if ( tcc_admin_options.showhide ) {
 		jQuery.each( tcc_admin_options.showhide, function( counter, item ) {
 			if ( targetableElement( item ) ) {
-				var origin = '.' + item.origin + ' input:radio';
+				let origin = '.' + item.origin + ' input:radio';
 				jQuery( origin ).change( item, function( e ) {
 					targetableElement( e.data );
 				});
@@ -18,11 +18,11 @@ jQuery( document ).ready( function() {
 });
 
 function imageDelete( el ) {
-	var ans = confirm( 'Remove this image?' ); // FIXME: localize this
+	let ans = confirm( 'Remove this image?' ); // FIXME: localize this
 	if ( ans ) {
-		var iuField = jQuery( el.parentNode ).data( 'field' );
-		var iuInput = document.getElementById( iuField + '_input' );
-		var iuImage = document.getElementById( iuField + '_img' );
+		let iuField = jQuery( el.parentNode ).data( 'field' );
+		let iuInput = document.getElementById( iuField + '_input' );
+		let iuImage = document.getElementById( iuField + '_img' );
 		iuInput.value = '';
 		iuImage.src   = '';
 		jQuery( iuImage ).addClass( 'hidden' );
@@ -31,19 +31,19 @@ function imageDelete( el ) {
 
 function imageUploader( el, e ) {
 	e.preventDefault();
-	var iuTitle  = jQuery( el.parentNode ).data( 'title' );
-	var iuButton = jQuery( el.parentNode ).data( 'button' );
-	var iuField  = jQuery( el.parentNode ).data( 'field' );
-	var custom_uploader = wp.media({
+	let iuTitle  = jQuery( el.parentNode ).data( 'title' );
+	let iuButton = jQuery( el.parentNode ).data( 'button' );
+	let iuField  = jQuery( el.parentNode ).data( 'field' );
+	const custom_uploader = wp.media({
 		title: iuTitle,
 		button: { text: iuButton, },
 		multiple: false
 	});
 	custom_uploader.on( 'select', function() {
-		var attachment = custom_uploader.state().get( 'selection' ).first().toJSON();
+		let attachment = custom_uploader.state().get( 'selection' ).first().toJSON();
 		if ( iuField ) {
-			var iuInput = document.getElementById( iuField + '_input' );
-			var iuImage = document.getElementById( iuField + '_img' );
+			let iuInput = document.getElementById( iuField + '_input' );
+			let iuImage = document.getElementById( iuField + '_img' );
 			iuInput.value = attachment.url;
 			iuImage.src   = attachment.url;
 			jQuery( el.parentNode ).children( '.form-image-container' ).removeClass( 'hidden' );
@@ -55,12 +55,12 @@ function imageUploader( el, e ) {
 
 function showhideElements( els ) {
 	jQuery( els ).each( function( el ) {
-		var target = jQuery( el ).attr( 'data-item' );
-		var show   = jQuery( el ).attr( 'data-show' );
+		let target = jQuery( el ).attr( 'data-item' );
+		let show   = jQuery( el ).attr( 'data-show' );
 		if ( show ) {
 			showhideAdminElement( el, target, show, null );
 		}
-		var hide = jQuery( el ).attr( 'data-hide' );
+		let hide = jQuery( el ).attr( 'data-hide' );
 		if ( hide ) {
 			showhideAdminElement( el, target, null, hide );
 		}
@@ -73,9 +73,9 @@ function targetableElement( item ) {
 
 function showhideAdminElement( origin, target, show, hide ) {
 	if ( origin && target ) {
-		var radio = jQuery( origin + ' input:radio:checked' );
+		let radio = jQuery( origin + ' input:radio:checked' );
 		if ( radio.length ) {
-			var state = jQuery( radio ).val();
+			let state = jQuery( radio ).val();
 			if ( state ) {
 				if ( show ) {
 					if ( state === show ) {
--- js/tax_nodelete.js
+++ js/tax_nodelete.js
@@ -1,6 +1,6 @@
 
 jQuery(document).ready(function() {
-  for(var i=0,cnt=term_list.length;i<cnt;i++) {
+  for( let i = 0, cnt = term_list.length; i < cnt; i++ ) {
     jQuery('#tag-'+term_list[i]+' .row-actions .delete').remove();
     jQuery('#tag-'+term_list[i]+' label').remove();
     jQuery('#tag-'+term_list[i]+' input').remove();
